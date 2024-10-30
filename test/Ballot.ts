26c34983ce6bc18b0e4010afecdba904fc1830a8
Mohammad Amir <amir@connextar.com>
2024-10-30 17:46:25 +0000

    Finished implementing the tests.

--- test/Ballot.ts
+++ test/Ballot.ts
@@ -1,7 +1,7 @@
-import { expect } from "chai";
-import { toHex, hexToString } from "viem";
-import { viem } from "hardhat";
-import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import {expect} from "chai";
+import {toHex, hexToString} from "viem";
+import {viem} from "hardhat";
+import {loadFixture} from "@nomicfoundation/hardhat-network-helpers";
 import {PublicClient, WalletClient} from "@nomicfoundation/hardhat-viem/src/types";
 
 const CONTRACT_NAME = "Ballot";
@@ -19,9 +19,9 @@ const deployContract = async () => {
   const deployerAddress = deployer.account.address;
   const otherAddress = otherAccount.account.address;
   const ballotContract = await viem.deployContract(CONTRACT_NAME, [
-    PROPOSALS.map((prop) => toHex(prop, { size: 32 })),
+    PROPOSALS.map((prop) => toHex(prop, {size: 32})),
   ]);
-  return { publicClient, deployer, deployerAddress, otherAccount, otherAccount2, otherAddress, ballotContract };
+  return {publicClient, deployer, deployerAddress, otherAccount, otherAccount2, otherAddress, ballotContract};
 };
 
 const giveRightToVoteFixture = async (publicClient: PublicClient, ballotContract: any, toAddress: string) => {
@@ -30,7 +30,7 @@ const giveRightToVoteFixture = async (publicClient: PublicClient, ballotContract
     toAddress,
   ]);
   // https://viem.sh/docs/actions/public/getTransactionReceipt#gettransactionreceipt
-  const receipt = await publicClient.getTransactionReceipt({ hash: txHash });
+  const receipt = await publicClient.getTransactionReceipt({hash: txHash});
   // console.log("Ballot -> giveRightToVoteFixture -> txHash", txHash, "receipt", receipt);
   // https://viem.sh/docs/glossary/terms#transaction-receipt
   expect(receipt.status).to.equal("success");
@@ -39,15 +39,18 @@ const giveRightToVoteFixture = async (publicClient: PublicClient, ballotContract
   expect(otherVoter[VOTER_WEIGHT_IDX]).to.eq(1n);
 };
 
-const voteFixture = async (ballotContract: any, proposalIdx: number, account: WalletClient) => {
+const voteFixture = async (ballotContract: any, proposalIdx: number, account: WalletClient, assertZeroVotes = true) => {
   // Verify that the proposal has zero votes to begin with.
-  const proposal = await ballotContract.read.proposals([BigInt(proposalIdx)]);
-  expect(proposal[PROPOSAL_VOTES_IDX]).to.eq(0n);
+  if (assertZeroVotes) {
+    const proposal = await ballotContract.read.proposals([BigInt(proposalIdx)]);
+    expect(proposal[PROPOSAL_VOTES_IDX]).to.eq(0n);
+  }
+
   // Create contract as the account to vote
   const contractAsAccount = await viem.getContractAt(
     CONTRACT_NAME,
     ballotContract.address,
-    { client: { wallet: account } }
+    {client: {wallet: account}}
   );
   // Verify that the vote succeeds
   // https://www.chaijs.com/plugins/chai-as-promised/
@@ -69,7 +72,7 @@ const delegateFixture = async (publicClient: PublicClient, ballotContract: any, 
   expect(targetWeight).to.eq(1n);
   // Delegate to the target voter
   const txHash = await ballotContract.write.delegate([toAddress]);
-  const receipt = await publicClient.getTransactionReceipt({ hash: txHash });
+  const receipt = await publicClient.getTransactionReceipt({hash: txHash});
   // console.log("Ballot -> delegateFixture -> txHash", txHash, "receipt", receipt);
   expect(receipt.status).to.equal("success");
   // Get the updated weights for the target voter
@@ -84,17 +87,17 @@ const delegateFixture = async (publicClient: PublicClient, ballotContract: any, 
 describe(CONTRACT_NAME, async () => {
   describe("when the contract is deployed", async () => {
     it("has the provided proposals", async () => {
-      const { ballotContract } = await loadFixture(deployContract);
+      const {ballotContract} = await loadFixture(deployContract);
 
       for (let index = 0; index < PROPOSALS.length; index++) {
         const proposal = await ballotContract.read.proposals([BigInt(index)]);
         // console.log("Ballot -> deployed -> proposals -> index", index, "proposal", proposal);
-        expect(hexToString(proposal[PROPOSAL_NAME_IDX], { size: 32 })).to.eq(PROPOSALS[index]);
+        expect(hexToString(proposal[PROPOSAL_NAME_IDX], {size: 32})).to.eq(PROPOSALS[index]);
       }
     });
 
     it("has zero votes for all proposals", async () => {
-      const { ballotContract } = await loadFixture(deployContract);
+      const {ballotContract} = await loadFixture(deployContract);
       for (let index = 0; index < PROPOSALS.length; index++) {
         const proposal = await ballotContract.read.proposals([BigInt(index)]);
         expect(proposal[PROPOSAL_VOTES_IDX]).to.eq(0n);
@@ -102,7 +105,7 @@ describe(CONTRACT_NAME, async () => {
     });
 
     it("sets the deployer address as chairperson", async () => {
-      const { ballotContract, deployer } = await loadFixture(deployContract);
+      const {ballotContract, deployer} = await loadFixture(deployContract);
       const chairperson = await ballotContract.read.chairperson();
       const deployerAddress = deployer.account.address;
       // console.log("Ballot -> deployed -> chairperson -> chairperson", chairperson, "deployer", deployerAddress);
@@ -110,7 +113,7 @@ describe(CONTRACT_NAME, async () => {
     });
 
     it("sets the voting weight for the chairperson as 1", async () => {
-      const { ballotContract } = await loadFixture(deployContract);
+      const {ballotContract} = await loadFixture(deployContract);
       const chairperson = await ballotContract.read.chairperson();
       const chairpersonVoter = await ballotContract.read.voters([chairperson]);
       console.log("Ballot -> deployed -> voting weights -> chairpersonVoter", chairpersonVoter);
@@ -120,13 +123,13 @@ describe(CONTRACT_NAME, async () => {
 
   describe("when the chairperson interacts with the giveRightToVote function in the contract", async () => {
     it("gives right to vote for another address", async () => {
-      const { publicClient, ballotContract, deployer, otherAddress } = await loadFixture(deployContract);
+      const {publicClient, ballotContract, deployer, otherAddress} = await loadFixture(deployContract);
       // Grant right to vote
       await giveRightToVoteFixture(publicClient, ballotContract, otherAddress);
     });
 
     it("can not give right to vote for someone that has voted", async () => {
-      const { publicClient, ballotContract, otherAccount, otherAddress } = await loadFixture(deployContract);
+      const {publicClient, ballotContract, otherAccount, otherAddress} = await loadFixture(deployContract);
       // Grant right to vote
       await giveRightToVoteFixture(publicClient, ballotContract, otherAddress);
       // Make voter vote
@@ -138,7 +141,7 @@ describe(CONTRACT_NAME, async () => {
     });
 
     it("can not give right to vote for someone that has already voting rights", async () => {
-      const { publicClient, ballotContract, otherAddress } = await loadFixture(deployContract);
+      const {publicClient, ballotContract, otherAddress} = await loadFixture(deployContract);
       // Grant right to vote
       await giveRightToVoteFixture(publicClient, ballotContract, otherAddress);
       // Assign to same voter again and expect error.
@@ -174,11 +177,11 @@ describe(CONTRACT_NAME, async () => {
 
   describe("when an account other than the chairperson interacts with the giveRightToVote function in the contract", async () => {
     it("should revert", async () => {
-      const { ballotContract, deployerAddress, otherAccount } = await loadFixture(deployContract);
+      const {ballotContract, deployerAddress, otherAccount} = await loadFixture(deployContract);
       const contractAsOtherAccount = await viem.getContractAt(
         CONTRACT_NAME,
         ballotContract.address,
-        { client: { wallet: otherAccount } }
+        {client: {wallet: otherAccount}}
       );
       await expect(
         contractAsOtherAccount.write.giveRightToVote([deployerAddress])
@@ -188,11 +191,11 @@ describe(CONTRACT_NAME, async () => {
 
   describe("when an account without right to vote interacts with the vote function in the contract", async () => {
     it("should revert", async () => {
-      const { ballotContract, otherAccount } = await loadFixture(deployContract);
+      const {ballotContract, otherAccount} = await loadFixture(deployContract);
       const contractAsOtherAccount = await viem.getContractAt(
         CONTRACT_NAME,
         ballotContract.address,
-        { client: { wallet: otherAccount } }
+        {client: {wallet: otherAccount}}
       );
       await expect(
         contractAsOtherAccount.write.vote([BigInt(0)])
@@ -202,11 +205,11 @@ describe(CONTRACT_NAME, async () => {
 
   describe("when an account without right to vote interacts with the delegate function in the contract", async () => {
     it("should revert", async () => {
-      const { ballotContract, deployerAddress, otherAccount } = await loadFixture(deployContract);
+      const {ballotContract, deployerAddress, otherAccount} = await loadFixture(deployContract);
       const contractAsOtherAccount = await viem.getContractAt(
         CONTRACT_NAME,
         ballotContract.address,
-        { client: { wallet: otherAccount } }
+        {client: {wallet: otherAccount}}
       );
       await expect(
         contractAsOtherAccount.write.delegate([deployerAddress])
@@ -216,7 +219,7 @@ describe(CONTRACT_NAME, async () => {
 
   describe("when someone interacts with the winningProposal function before any votes are cast", async () => {
     it("should return 0", async () => {
-      const { ballotContract } = await loadFixture(deployContract);
+      const {ballotContract} = await loadFixture(deployContract);
       const proposalIdx = await ballotContract.read.winningProposal();
       expect(proposalIdx).to.equal(0n);
     });
@@ -224,7 +227,7 @@ describe(CONTRACT_NAME, async () => {
 
   describe("when someone interacts with the winningProposal function after one vote is cast for the first proposal", async () => {
     it("should return 0", async () => {
-      const { ballotContract, deployer } = await loadFixture(deployContract);
+      const {ballotContract, deployer} = await loadFixture(deployContract);
       // Cast vote for the first proposal.
       const projectIdx = 0;
       await voteFixture(ballotContract, projectIdx, deployer);
@@ -236,7 +239,7 @@ describe(CONTRACT_NAME, async () => {
 
   describe("when someone interacts with the winnerName function before any votes are cast", async () => {
     it("should return name of proposal 0", async () => {
-      const { ballotContract } = await loadFixture(deployContract);
+      const {ballotContract} = await loadFixture(deployContract);
       const winnerName = await ballotContract.read.winnerName();
       const proposal = await ballotContract.read.proposals([BigInt(0)]);
       expect(winnerName).to.equal(proposal[PROPOSAL_NAME_IDX]);
@@ -245,7 +248,7 @@ describe(CONTRACT_NAME, async () => {
 
   describe("when someone interacts with the winnerName function after one vote is cast for the first proposal", async () => {
     it("should return name of proposal 0", async () => {
-      const { ballotContract, deployer } = await loadFixture(deployContract);
+      const {ballotContract, deployer} = await loadFixture(deployContract);
       // Cast vote for the first proposal.
       const projectIdx = 0;
       await voteFixture(ballotContract, projectIdx, deployer);
@@ -257,9 +260,39 @@ describe(CONTRACT_NAME, async () => {
   });
 
   describe("when someone interacts with the winningProposal function and winnerName after 5 random votes are cast for the proposals", async () => {
-    // TODO
     it("should return the name of the winner proposal", async () => {
-      throw Error("Not implemented");
+      const {publicClient, ballotContract} = await loadFixture(deployContract);
+      const [deployer, account1, account2, account3, account4] = await viem.getWalletClients();
+      // Store the proposal names.
+      const totalProposals = PROPOSALS.length;
+      const proposalNames: { [key: string]: string } = {};
+      const votes: { [key: string]: number } = {};
+
+      for (let p = 0; p < totalProposals; p++) {
+        const proposal = await ballotContract.read.proposals([BigInt(p)]);
+        proposalNames["idx-" + p] = proposal[PROPOSAL_NAME_IDX];
+        votes["idx-" + p] = 0;
+      }
+
+      // First random vote
+      let randProposalIdx = Math.floor(Math.random() * totalProposals);
+      await voteFixture(ballotContract, randProposalIdx, deployer);
+      votes["idx-" + randProposalIdx]++;
+
+      // Grant the additional accounts the right to vote and cast random votes with them.
+      for (const account of [account1, account2, account3, account4]) {
+        await giveRightToVoteFixture(publicClient, ballotContract, account.account.address);
+        randProposalIdx = Math.floor(Math.random() * totalProposals);
+        await voteFixture(ballotContract, randProposalIdx, account, false);
+        votes["idx-" + randProposalIdx]++;
+      }
+
+      // Winning proposal name should match the locally calculated winner
+      const winningIdx = Object.keys(votes).reduce((a, b) => votes[a] > votes[b] ? a : b);
+      const winningProposalName = proposalNames[winningIdx];
+      const winnerName = await ballotContract.read.winnerName();
+      console.log("Ballot -> verify random winner -> proposalNames", proposalNames, "votes", votes, "winningIdx", winningIdx, "winningProposalName", winningProposalName, "winnerName", winnerName);
+      expect(winnerName.toLowerCase()).to.equal(winningProposalName.toLowerCase());
     });
   });
 });
